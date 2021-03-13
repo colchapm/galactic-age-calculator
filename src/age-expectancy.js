bc357c6c048a32e1a0a999ec7f1fdf6572d4cb9d
Collin Chapman <cchap14@gmail.com>
2021-03-13 13:11:56 -0800

    create test one, return average life expectancy based on region

---
+++ src/age-expectancy.js
@@ -0,0 +1,48 @@
+//Business Logic
+
+export default class AgeExpectancy {
+  constructor (region, gender, tobacco, alcohol) {
+    this.region = region;
+    this.gender = gender;
+    this.tobacco = tobacco;
+    this.alcohol = alcohol;
+    this.ageExpectancy = 0;
+  }
+
+  regionCalculator() {
+    if (this.region === "Americas") {
+      this.ageExpectancy = 77;
+    } 
+    else if (this.region === "Africa") {
+      this.ageExpectancy = 64;
+    } 
+    else if (this.region === "Eastern Mediterranean") {
+      this.ageExpectancy = 70;
+    } 
+    else if (this.region === "Europe") {
+      this.ageExpectancy = 78;
+    } 
+    else if (this.region === "Western Pacific") {
+      this.ageExpectancy = 78;
+    } 
+    else {
+      this.ageExpectancy = 71;
+    }
+    return this.ageExpectancy;
+  }
+
+
+}
+
+
+//User Interface Logic
+
+// $(document).ready(function() {
+//   $("form#userInput").submit(function(event)
+//     event.preventDefault();
+//     let age = $("#userAge").val();
+//     let region = $("#region").val();
+//     let gender = $("#gender").val();
+//     let tobacco = $("#tobacco").val();
+//     let alcohol = $("#alcohol").val();
+// })
