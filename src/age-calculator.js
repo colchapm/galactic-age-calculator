f24006a74afd3b17b50c8a515205bddf8a2af359
Collin Chapman <cchap14@gmail.com>
2021-03-13 09:40:33 -0800

    refactor to make code more DRY

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -5,39 +5,32 @@ export default class AgeCalculator {
 }
 
   earthCalculator() {
-    let earthAge = this.age * 1.0;
-    return earthAge;
+    return this.age * 1.0;
 }
 
   mercuryCalculator() {
-    let mercuryAge = Math.round(this.age * .24)
-    return mercuryAge;
+    return Math.round(this.age * .24)
   }
 
   venusCalculator() {
-    let venusAge = Math.round(this.age * .62)
-    return venusAge;
+    return Math.round(this.age * .62)
   }
 
   marsCalculator() {
-    let marsAge = Math.round(this.age * 1.88)
-    return marsAge;
+    return Math.round(this.age * 1.88)
   }
 
   jupiterCalculator() {
-    let jupiterAge = Math.round(this.age * 11.86)
-    return jupiterAge;
+    return Math.round(this.age * 11.86)
   }
 
   lifeCalculator() {
     if (this.age > this.expectancy) {
-      let yearsBeyond = Math.round(this.age - this.expectancy)
-      return (`You have lived ${yearsBeyond} years beyond average life expectancy on Earth.`);
+      return (`You have lived ${this.age - this.expectancy} years beyond average life expectancy on Earth.`);
     } else if (this.age === this.expectancy) {
       return (`You are ${this.age} years old and that is equal to the average life expectancy on Earth.`);
     } else {
-      let toGo = Math.round(this.expectancy - this.age)
-      return (`You have ${toGo} years left to live before reaching average life expectancy on Earth.`);
+      return (`You have ${this.expectancy - this.age} years left to live before reaching average life expectancy on Earth.`);
     }
     }
   }
