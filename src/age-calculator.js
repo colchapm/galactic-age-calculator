bc269f7ada6285f1732967f6c4f863aeca92027b
Collin Chapman <cchap14@gmail.com>
2021-03-13 07:07:51 -0800

    upgrade constuctor and prototype and refactor with class keyword

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,8 +1,11 @@
-export default function AgeCalculator(age) {
-  this.age = age;
+export default class AgeCalculator {
+  constructor(age) {
+    this.age = age;
 }
 
-AgeCalculator.prototype.earthCalculator = function () {
-  let earthAge = this.age * 1.0;
-  return earthAge;
+  earthCalculator() {
+    let earthAge = this.age * 1.0;
+    return earthAge;
+}
+
 }
