ccf966b544613033471fa588fdd71905ce2c1ce8
Collin Chapman <cchap14@gmail.com>
2021-03-13 06:41:53 -0800

    identify file dependencies with import/export

---
+++ __tests__/age-calculator.test.js
@@ -0,0 +1,20 @@
+import AgeCalculator from '../src/age-calculator.js';
+
+describe ('Test', () => {
+  let newTest; 
+  beforeEach(() => {
+    let age = 30;
+    newTest = new AgeCalculator(age);
+  });
+
+  
+
+
+
+
+
+
+
+
+
+})
---
+++ src/age-calculator.js
@@ -0,0 +1,4 @@
+export default function AgeCalculator(age) {
+  this.planet = planet;
+  this.age = age;
+}
