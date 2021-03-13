a9735f43716201d5187a35968a82e63cdef8cbf2
Collin Chapman <cchap14@gmail.com>
2021-03-13 13:09:39 -0800

    add age expectancy test file and define test 1, return average life expectancy depending on region

---
+++ __tests__/age-expectancy.test.js
@@ -0,0 +1,22 @@
+import AgeExpectancy from '../src/age-expectancy.js';
+
+describe ('Test', () => {
+  let expectancyTest;
+  beforeEach(() => {
+    let region = "Americas";
+    let gender = "Male";
+    let tobacco = "Socially";
+    let alcohol = "2-5 Drinks Per Week";
+    // let region = $("#region").val();
+    // let gender = $("#gender").val();
+    // let tobacco = $("#tobacco").val();
+    // let alcohol = $("#alcohol").val();
+    expectancyTest = new AgeExpectancy(region, gender, tobacco, alcohol)
+  });
+
+  test('should return average life expectancy based on region', () => {
+    expect(expectancyTest.regionCalculator()).toEqual(77);
+  })
+
+
+})
